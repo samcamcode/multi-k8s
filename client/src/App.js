938a8580d0592dff8b878de193b0c99950429113
samcamcode <samcamcode@yahoo.com>
2020-11-15 01:53:40 -0800

    changes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
           >
             Learn React
           </a>
-          <Link to="/">Fib Calculator</Link>
+          <Link to="/">Fib Calculator Ver. Kubernetes</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
